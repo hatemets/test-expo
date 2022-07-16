7135ad342f4d57d294b777a95614f1413548598e
hatemets <mistermark009@gmail.com>
2022-07-16 14:06:58 +0300

    Stack navigator

--- App.js
+++ App.js
@@ -1,36 +1,47 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-function AboutScreen() {
+function AboutScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>About Screen</Text>
+        <Button title="home" onPress={() => navigation.navigate("home")} />
+        <Button title="contact" onPress={() => navigation.navigate("contact")} />
     </View>
   );
 }
 
-function ContactScreen() {
+function ContactScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Contact Screen</Text>
+        <Button title="about" onPress={() => navigation.navigate("about")} />
+        <Button title="home" onPress={() => navigation.navigate("home")} />
     </View>
   );
 }
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+        <Button title="about" onPress={() => navigation.navigate("about")} />
+        <Button title="contact" onPress={() => navigation.navigate("contact")} />
     </View>
   );
 }
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="home" component={HomeScreen} />
+                <Stack.Screen name="about" component={AboutScreen} />
+                <Stack.Screen name="contact" component={ContactScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
